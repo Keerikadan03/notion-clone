4b72751e783bb80c3b5029eca3ae7b6b558201ac
Keerikadan03 <fenseraju@gmail.com>
2023-12-18 22:24:58 +0530

    added landing page elements to the page

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -7,8 +7,8 @@ import React from 'react'
 export const Heading = () => {
   return (
     <div className='max-w-3xl space-y-4'>
-        <h1 className='text-3xl sm: text-5xl md:text-6xl'>Write, plan, share. With AI at your side.
-        <span className='underline'>Notion</span>
+        <h1 className='text-3xl sm: text-5xl md:text-6xl'>Write, plan, share.<br/> With AI at your side.
+        {/* <span className='underline'>Notion</span> */}
         </h1>
         <h3 className='text-base sm: text-xl md:text-2xl font-medium'>
         Notion is the connected workspace where <br/> better, faster work happens.
