ff54930f986ec15e1b669e93844f0cf2d116b3f5
Keerikadan03 <fenseraju@gmail.com>
2023-12-21 14:11:11 +0530

    faced an error stating that the Could not find ConvexProviderWithAuth (or ConvexProviderWithClerk or ConvexProviderWithAuth0) as an ancestor component. This component may be missing, or you might have two instances of the convex/react module loaded in your project. so reverting back to previous commit

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { useConvexAuth } from "convex/react";
-import { SignIn, SignInButton, UserButton } from "@clerk/clerk-react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
 import { cn } from "@/lib/utils";
 import { Logo } from "./Logo";
 import { ModeToggle } from "@/components/mode-toggle";
@@ -10,8 +10,9 @@ import { Spinner } from "@/components/spinner";
 import Link from "next/link";
 
 const Navbar = () => {
-    const {isAuthenticated, isLoading} = useConvexAuth();
-    const scrolled = useScrollTop();
+  
+  const {isAuthenticated, isLoading} = useConvexAuth();
+  const scrolled = useScrollTop();
     
   return (
     <div className={cn("z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm ")}>
